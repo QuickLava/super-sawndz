1dd40485000ab2b4ab8fcad857982c4d3b87dd5a
QuickLava <fastmovingvolcanomatter@gmail.com>
2022-06-08 03:36:38 -0700

    Updated Process Name, Removed PDB Path

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "BrawlSoundConverter" )]
+[assembly: AssemblyTitle("Super Sawndz")]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "BrawlSoundConverter" )]
-[assembly: AssemblyCopyright( "Copyright ©  2012" )]
+[assembly: AssemblyProduct("Super Sawndz")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
