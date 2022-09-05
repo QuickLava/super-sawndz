d01d67e73e0b6ee154514010321314ba4ec0fd36
QuickLava <61454308+QuickLava@users.noreply.github.com>
2022-09-04 21:57:31 -0700

    Incremented Version Number

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,7 +61,7 @@ namespace BrawlSoundConverter.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.8.0.
+        ///   Looks up a localized string similar to 1.9.0.
         /// </summary>
         internal static string Version {
             get {
