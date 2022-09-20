a427313c03a93c49c36a855b935246adc579de08
QuickLava <61454308+QuickLava@users.noreply.github.com>
2022-09-19 19:14:46 -0700

    Settings Can Now Be Reset Via Command Line Arg
    If you pass the command line argument "reset", the program will force all of its settings back to their default values before running. Useful if you somehow break your tab settings, and the program won't load far enough for you to fix them.

--- Program.cs
+++ Program.cs
@@ -11,12 +11,19 @@ namespace BrawlSoundConverter
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main()
+		static void Main(string[] args)
 		{
 			try
 			{
 				Application.EnableVisualStyles();
 				Application.SetCompatibleTextRenderingDefault(false);
+				if (args.Length > 0)
+				{
+					if (args[0].ToLower() == "reset")
+					{
+						Properties.Settings.Default.Reset();
+					}
+				}
 				Application.Run(new Form1());
 			}
 			catch (Exception)
